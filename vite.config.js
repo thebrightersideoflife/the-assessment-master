cf54b80247155cd315edc27089bc9097d0facac5
The Brighter Side of Life <thebrightersideoflife.tbsol@gmail.com>
2025-10-16 15:36:34 +0200

    Vercel code reconfigured back

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default defineConfig({
+export default {
+  base: "/",
   plugins: [react()],
-  build: { outDir: 'dist' }
-})
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, "./src/components"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      hooks: path.resolve(__dirname, "./src/hooks"),
+      utils: path.resolve(__dirname, "./src/utils"),
+      store: path.resolve(__dirname, "./src/store"),
+      data: path.resolve(__dirname, "./src/data"),
+      styles: path.resolve(__dirname, "./src/styles"),
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 3000,
+  },
+};
